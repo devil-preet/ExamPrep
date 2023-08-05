aff4ab581085e889aaeaa8f6eb73ec4dc8b358a0
devil-preet <anmolpreetkaurchouhan@gmail.com>
2023-08-05 23:30:03 +0530

    done

---
+++ lib/page/PG/MSc_IT/MSc_it.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class msc_it extends StatefulWidget {
+  const msc_it({super.key});
+
+  @override
+  State<msc_it> createState() => _msc_itState();
+}
+
+class _msc_itState extends State<msc_it> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
